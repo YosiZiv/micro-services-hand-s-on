9687a11b548cdec9d307ab305942a6354f9c8792
unknown <yosizivjs@gmail.com>
2020-07-08 01:05:09 +0300

    init

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,8 @@
+import { Message } from "node-nats-streaming";
+import { Listener, OrderCreatedEvent, Subjects } from "@yztickets/common";
+import { queueGroupName } from "./queue-group-name";
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
